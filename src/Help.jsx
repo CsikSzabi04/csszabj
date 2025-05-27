2344507a32a14478ae39255b504e4112a5044f18
Cs_Szabj04 <68150404+CsikSzabi04@users.noreply.github.com>
2025-05-27 09:43:34 +0200

    Update Help.jsx

--- src/Help.jsx
+++ src/Help.jsx
@@ -191,22 +191,7 @@ set.remove(valami); // törlés a setből
 //Vesszővel 
 System.out.printf("3) A kígyók származási helye (abc): %s%n", String.join(", ", helyek));
 */`
-},
-
-{
-  title: "TreeSet + String-join(", ", x)",
-  tags: ["Konzol"],
-  code: `/*
-TreeSet<String> helyek = new TreeSet<>();
-  for (Adat x : adat) {
-     if (!helyek.contains(x.hely)) {
-         helyek.add(x.hely);
-     }
-  }
-  System.out.printf("3) A kígyók származási helye (abc): %s%n", String.join(", ", helyek));
-*/`
-}  
-
+}
 ];
 
 const Section = ({ label }) => {
